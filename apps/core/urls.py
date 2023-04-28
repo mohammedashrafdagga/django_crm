8ddb105204dbe3f2ac5193fbb4d0145a0cdc30b1
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-04-28 17:38:14 +0300

    create about page

--- apps/core/urls.py
+++ apps/core/urls.py
@@ -1,9 +1,10 @@
-from .views import base_page
+from .views import index_page, about_page
 from django.urls import path
 
 
 # naming app
 app_name = 'core'
 urlpatterns = [
-    path('', base_page, name='base')
+    path('', index_page, name='index'),
+    path('about/', about_page, name='about_page')
 ]
--- apps/core/views.py
+++ apps/core/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
 
-def base_page(request):
-    return render(request, 'base.html')
+def index_page(request):
+    return render(request, 'core/index.html')
+
+
+def about_page(request):
+    return render(request, 'core/about.html')
