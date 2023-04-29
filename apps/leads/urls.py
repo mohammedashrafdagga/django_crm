80e34132d781d5b6b6389555b3fef981f274b8b7
Mohammed Dagga <47271587+mohammedashrafdagga@users.noreply.github.com>
2023-04-29 11:30:11 +0300

    create delete page with logic

--- apps/leads/urls.py
+++ apps/leads/urls.py
@@ -1,10 +1,11 @@
-from .views import lead_add, lead_list, lead_detail
+from .views import lead_add, lead_list, lead_detail, lead_delete
 from django.urls import path
 
 
 app_name = 'leads'
 urlpatterns = [
     path('', lead_list, name='list'),
-    path('detail/<int:pk>/', lead_detail, name='detail'),
+    path('<int:pk>/detail/', lead_detail, name='detail'),
+    path('<int:pk>/delete/', lead_delete, name='delete'),
     path('add/', lead_add, name='add'),
 ]
--- apps/leads/views.py
+++ apps/leads/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from .forms import AddLeadForm
 from .models import Lead
@@ -12,7 +13,8 @@ def lead_add(request):
             lead = form.save(commit=False)
             lead.create_by = request.user
             lead.save()
-            return redirect('dashboard:main')
+            messages.success(request, 'The Lead wes created successfully')
+            return redirect('leads:list')
     else:
         form = AddLeadForm()
     return render(request, 'leads/add.html', {'form': form})
@@ -28,3 +30,11 @@ def lead_list(request):
 def lead_detail(request, pk):
     lead = get_object_or_404(Lead, create_by=request.user, pk=pk)
     return render(request, 'leads/detail.html', {'lead': lead})
+
+
+@login_required
+def lead_delete(request, pk):
+    lead = get_object_or_404(Lead, create_by=request.user, pk=pk)
+    lead.delete()
+    messages.success(request, 'The Lead wes deleted.')
+    return redirect('leads:list')
